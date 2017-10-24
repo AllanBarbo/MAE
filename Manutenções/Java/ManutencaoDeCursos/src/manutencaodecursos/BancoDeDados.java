882d4ab316741376bb40d4b5a5be9a0ae69ed8e5
Matheus Quintão Santiago <matheusqs10000@hotmail.com.br>
2017-10-24 10:24:12 -0200

    Comentei e coloqueo no padrao esses projetos

--- Manutenções/Java/ManutencaoDeCursos/src/manutencaodecursos/BancoDeDados.java
+++ Manutenções/Java/ManutencaoDeCursos/src/manutencaodecursos/BancoDeDados.java
@@ -11,14 +11,18 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-/**
- *
- * @author Aluno
- */
+/*
+    Grupo:​ ​ MAE.
+    Data​ ​ de​ ​ modificação:​ ​ 24/10/2017.
+    Autor:​ ​Matheus Quintão Santiago.
+    ​Objetivo​ ​ da​ ​ modificação:​ ​ Padronizando o codigo e comentando.
+*/
 public class BancoDeDados<E> {
     private com.mysql.jdbc.Connection link = null;
     
     public BancoDeDados() throws SQLException{
+        
+        //Testa se o driver de JDBC para Mysql esta no projeto.
         try{
             Class.forName("com.mysql.jdbc.Driver");
         }
@@ -26,6 +30,8 @@ public class BancoDeDados<E> {
             System.err.println("Driver não encontrado.");
         }
         System.err.println("Driver encontrado com sucesso!");
+        
+        //Faz a conexão com o banco de dados.
         link = (com.mysql.jdbc.Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/educatio", "root", "");
         if (link != null){
             System.err.println("Conexão realizada com sucesso!");
@@ -34,6 +40,7 @@ public class BancoDeDados<E> {
         }
     }
     
+    // Método para selecionar os registros de uma tabela que o usuario queira.
     public ResultSet selecionarRegistros(String tabela, String pesquisa, String pesquisado) throws SQLException{
         Statement comando = link.createStatement();
         String query = "SELECT * FROM `" + tabela + "` WHERE " + pesquisa + " = \'" + pesquisado + "\'";
@@ -41,11 +48,10 @@ public class BancoDeDados<E> {
         return resultado;
     }  
    
+    // Método para criar um curso.
     public void criaCurso(int idDepto, String nome, int horasTotal, String modalidade) throws SQLException{
         Statement comando = link.createStatement();
-        PreparedStatement co = link.prepareStatement("INSERT INTO `cursos` (`idDepto`, `nome`, `horasTotal`, `modalidade`, `ativo`) VALUES (?,?,?,?,?,'S')");
-        
-        //String query = "INSERT INTO `cursos` (`id`, `idDepto`, `nome`, `horasTotal`, `modalidade`, `ativo`) VALUES ('" + id + "', '" + idDepto + "', '" + nome + "', '" + horasTotal + "', '" + modalidade + "' 'S')";
+        PreparedStatement co = link.prepareStatement("INSERT INTO `cursos` (`idDepto`, `nome`, `horasTotal`, `modalidade`, `ativo`) VALUES (?,?,?,?,'S')");
         co.setInt(1,idDepto);
         co.setString(2, nome);
         co.setInt(3, horasTotal);
@@ -53,12 +59,14 @@ public class BancoDeDados<E> {
         co.execute();
     }
     
+    // Método para deletar um curso.
     public void apagaCurso(int id) throws SQLException{
         Statement comando = link.createStatement();
         String query = "UPDATE `cursos` SET `ativo` = 'N' WHERE `id` = " + id;
         comando.executeUpdate(query);
     }
     
+    // Método para alterar um curso.
     public void alteraCurso(String item, E valorItemNovo, E valorItemAntigo) throws SQLException{
         Statement comando = link.createStatement();
         String query = "UPDATE `cursos` SET " + item + " = '" + valorItemNovo + "' WHERE " + item + " = '" + valorItemAntigo + "'";
--- Manutenções/Java/ManutencaoDeCursos/src/manutencaodecursos/ManutencaoDeCursos.java
+++ Manutenções/Java/ManutencaoDeCursos/src/manutencaodecursos/ManutencaoDeCursos.java
@@ -9,74 +9,108 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-/**
- *
- * @author Aluno
- */
+/*
+    Grupo:​ ​ MAE.
+    Data​ ​ de​ ​ modificação:​ ​ 24/10/2017.
+    Autor:​ ​Matheus Quintão Santiago.
+    ​Objetivo​ ​ da​ ​ modificação:​ ​ Padronizando o codigo e comentando.
+*/
 public class ManutencaoDeCursos {
 
-    /**
-     * @param args the command line arguments
-     * @throws java.sql.SQLException
-     */
     public static void main(String[] args) throws SQLException {
-        // TODO code application logic here
+        
+        // Instancia o Banco de Dados.
         BancoDeDados bancoDeDados = new BancoDeDados();
+        
+        // Dou opcao do usuario deletar, criar ou alterar um curso (primeiro switch).
         System.out.println("Digite 1 para criar, 2 para apagar e 3 para alterar um curso: ");
         Scanner leitor = new Scanner(System.in);
         switch(leitor.nextInt()){
+            
+            //Primeiro caso cria um curso.
             case 1:
+                
+                // Recebo o nome do novo curso.
                 System.out.println("\n"+"Digite o nome do curso: ");
                 String nome = leitor.next();
-
+                
+                // Recebo o departamento do novo curso.
                 System.out.println("\n"+"Digite o id do departamento do curso: ");
                 int idDepto = leitor.nextInt();
                 
+                // Recebo a quantidade de horas totais do novo curso.
                 System.out.println("\n"+"Digite a quantidade de horas total do curso: ");
                 int horasTotal = leitor.nextInt();
                 
+                // Recebo a modalidade do novo curso.
                 System.out.println("\n"+"Digite a modalidade do curso: ");
                 String modalidade = leitor.next();
                 
+                // Chamo o metodo criaCurso() da classe BancoDeDados.
                 bancoDeDados.criaCurso(idDepto, nome, horasTotal, modalidade);
                 break;
+            
+            // Segundo caso deleta um curso.
             case 2:
+                
+                // Recebo o id do curso que vai ser deletado.
                 System.out.println("\n"+"Digite o id do curso: ");
                 int id = leitor.nextInt();
                 
+                // Chamo o metodo apagaCurso() da classe BancoDeDados.
                 bancoDeDados.apagaCurso(id);
                 break;
                 
+            // Terceiro caso altera um curso.
             case 3:
                 
+                // Recebo o nome do curso para poder selecionar os dados que se quer alterar.
                 System.out.println("Digite o nome do curso: ");
                 nome = leitor.next();
+                
+                // Seleciona os registros da tabela cursos onde o nome do curso é igual a que o usuario informou.
                 ResultSet resultado = bancoDeDados.selecionarRegistros("cursos", "nome", nome);
                 resultado.first();
+                
+                // Verifica se o curso ja foi apagado.
                 if("S".equals(resultado.getString("ativo"))){
-                    System.out.println("Digite 1 para alterar o id, 2 para o id do curso, 3 para o nome do curso, 4 para a quantidade de horas totais e 5 para a modalidade do curso: ");
+                    
+                    // Dou opcao do usuario alterar o id, idDepto, nome, quantidade horas totais e modalidade do curso.
+                    System.out.println("Digite 1 para alterar o id, 2 para o id do departamento, 3 para o nome do curso, 4 para a quantidade de horas totais e 5 para a modalidade do curso: ");
                     resultado.first();
                     switch(leitor.nextInt()){
+                        
+                        // Primeiro caso altera o id do curso.
                         case 1:
                             System.out.println("Digite o novo valor: ");
                             bancoDeDados.alteraCurso("id", leitor.nextInt(), resultado.getString("id"));
                             break;
+                            
+                        // Segundo caso altera o id do departamento do curso.
                         case 2:
                             System.out.println("Digite o novo valor: ");
                             bancoDeDados.alteraCurso("idDepto", leitor.nextInt(), resultado.getString("idDepto"));
                             break;
+                            
+                        // Terceiro caso altera o nome do curso.
                         case 3:
                             System.out.println("Digite o novo valor: ");
                             bancoDeDados.alteraCurso("nome", leitor.next(), resultado.getString("nome"));
                             break;
+                            
+                        // Primeiro caso altera as horas totais do curso.
                         case 4:
                             System.out.println("Digite o novo valor: ");
                             bancoDeDados.alteraCurso("horasTotal", leitor.next(), resultado.getString("horasTotal"));
                             break;
+                            
+                        // Primeiro caso altera a modalidade do curso.
                         case 5:
                             System.out.println("Digite o novo valor: ");
                             bancoDeDados.alteraCurso("modalidade", leitor.next(), resultado.getString("modalidade"));
                             break;
+                            
+                        // Foi passado um valor diferente desses anteriores.
                         default:
                             System.out.println("Valor invalido.");
                     }

38572a89ded6d09d74b3811214ce5452be7e3e50
AllanBarbo <32453076+AllanBarbo@users.noreply.github.com>
2017-10-30 20:50:35 -0200

    Adiciona os Arquivos de manutenção de Turmas
    No MAE-Web-ManutencaoTurmas-Editar-BD.php, segundo case,( linha 30), ele não faz nada abaixo do: while($linha = $resultadoSELECT->fetch_array() )

---
+++ Manutenções/Web/MAE-Web-ManutencaoTurmas-SelecionarTurma-Excluir.php
@@ -0,0 +1,58 @@
+
+<!--
+Grupo:​ ​ MAE
+Data​ ​ de​ ​ modificação:​ ​ 09/10/2017
+Autor:​ ​ Allan Barbosa
+​ ​ ​ ​ ​ ​ ​ ​ ​Objetivo​ ​ da​ ​ modificação:​ Criação do script da manutenção de multas,parte de edição
+-->
+
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Bootstrap -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <title>Manutenção de turmas</title>
+
+    <!-- jQuery (plugins JavaScript do Bootstrap) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <!--Div container -->
+    <div class="container">
+        <h2><center>Manutenção de Turmas</center></h2><br>
+        <p>Caro(a) (Nome do biliotecário logado), Selecione a turma para exclusão </p>
+
+        <!--select com os nomes das turma -->
+          <form action="MAE-Web-ManutencaoTurmas-Excluir.php" method="post" >
+
+            <div class="form-group">
+              <label for="select">Selecione uma turma para excluir:</label>
+              <?php
+                $conn = new mysqli("localhost", "root", "","educatio");
+                $query = $conn->query(" SELECT * FROM `turmas` WHERE ativo='S' ORDER BY idCurso ASC,nome ASC");
+
+                //Cria o select dinamico pelo BD
+                echo " <select class='form-control' id='select' name='select[]'> ";
+                while($linha = $query->fetch_array() ) {
+                  echo " <option value = '".$linha["id"]."'> ".$linha["nome"]." - ID do curso: ".$linha["idCurso"]."</option> ";
+                }
+                echo "</select>";
+              ?>
+            </div>
+
+            <div class="form-group">
+              <button type="submit" class="btn btn-default">
+                <span class="glyphicon glyphicon-arrow-right"></span>  Enviar
+              </button>
+            </div>
+          </form>
+
+      </div>
+  </body>
+</html>
